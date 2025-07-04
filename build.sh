207056bba94f12bb3fe7d532f95e840e67767b2c
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-07-04 23:37:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
 # build
 /opt/crave/resync.sh
 
-rm -rf vendor/motorola/*
-rm -rf device/motorola/*
-rm -rf kernel/motorola/sm6375/*
+rm -rf vendor/motorola/fogo
+rm -rf vendor/motorola/sm6375-common 
+rm -rf device/motorola/fogo
+rm -rf device/motorola/sm6375-common 
+rm -rf kernel/motorola/sm6375
 
 git clone https://github.com/smiley9000/android_device_motorola_fogo device/motorola/fogo
 git clone https://github.com/smiley9000/vendor_fogo vendor/motorola
